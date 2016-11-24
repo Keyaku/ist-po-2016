af13cf2bb62e5ba51c3f2f3bd053fa45190f530b
Keyaku <antonio.luis.sarmento@gmail.com>
2016-11-24 16:44:08 +0000

    Implemented ReplaceExpression submenu
    Changes:
    - Implemented ReplaceExpression.execute()

--- pex/app/evaluator/ReplaceExpression.java
+++ pex/app/evaluator/ReplaceExpression.java
@@ -1,12 +1,12 @@
 package pex.app.evaluator;
 
-import pex.core.Interpreter;
-
-
 import pex.app.BadExpressionException;
 import pex.app.BadPositionException;
 
+import pex.parser.*;
+
 import pex.core.Program;
+import pex.core.expression.Expression;
 
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
@@ -26,6 +26,28 @@ public class ReplaceExpression extends ProgramCommand {
     /** @see pt.utl.ist.po.ui.Command#execute() */
     @Override
     public final void execute() throws BadExpressionException, BadPositionException {
-        //FIXME implement
+		Form f = new Form(title());
+		InputInteger inputPos = new InputInteger(f, Message.requestPosition());
+		InputString inputExp = new InputString(f, Message.requestExpression());
+		f.parse();
+
+		NewParser parser = new NewParser();
+		Expression exp = null;
+		int idx = inputPos.value();
+
+		// Check position value
+		if (!(0 <= idx && idx < entity().getNumExpressions())) {
+			throw new BadPositionException(idx);
+		}
+
+		// Attempt to parse expression
+		try {
+			exp = parser.parseString(inputExp.value(), entity());
+		} catch (BadSourceException|BadNumberException|InvalidExpressionException|MissingClosingParenthesisException|UnknownOperationException|EndOfInputException e) {
+			throw new BadExpressionException(inputExp.value());
+		}
+
+		// Finally, replace expression
+		entity().replace(idx, exp);
     }
 }
