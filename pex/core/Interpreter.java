238cc638f8d12d18157fc0a59c3d587521149bbd
Keyaku <antonio.luis.sarmento@gmail.com>
2016-12-08 04:34:41 +0000

    Reminding myself which methods to document

--- pex/core/Interpreter.java
+++ pex/core/Interpreter.java
@@ -22,14 +22,17 @@ public class Interpreter implements Serializable {
 		_io = appio;
 	}
 
+	// TODO: Document this method
 	public void setIdentifierValue(Identifier id, Literal value) {
 		_ids.put(id, value);
 	}
 
+	// TODO: Document this method
 	public Literal getIdentifierValue(Identifier id) {
 		return _ids.get(id);
 	}
 
+	// TODO: Document this method
 	public void addProgram(Program p) {
 		p.setInterpreter(this);
 		if (_progs.containsKey(p.getName())) {
@@ -38,18 +41,22 @@ public class Interpreter implements Serializable {
 		_progs.put(p.getName(), p);
 	}
 
+	// TODO: Document this method
 	public Program getProgram(String name) {
 		return _progs.get(name);
 	}
 
+	// TODO: Document this method
 	public void setFilename(String value) {
 		_filename = value;
 	}
 
+	// TODO: Document this method
 	public String getFilename() {
 		return _filename;
 	}
 
+	// TODO: Document this method
 	public AppIO getAppIO() {
 		return _io;
 	}
--- pex/core/Program.java
+++ pex/core/Program.java
@@ -16,39 +16,48 @@ public class Program implements Serializable {
 		_name = name;
 	}
 
+	// TODO: Document this method
 	public void setInterpreter(Interpreter value) {
 		_parent = value;
 	}
 
+	// TODO: Document this method
 	public Interpreter getInterpreter() {
 		return _parent;
 	}
 
+	// TODO: Document this method
 	public String getName() {
 		return _name;
 	}
 
+	// TODO: Document this method
 	public void add(int idx, Expression exp) {
 		_exps.add(idx, exp);
 	}
 
+	// TODO: Document this method
 	public void replace(int idx, Expression exp) {
 		_exps.set(idx, exp);
 	}
 
+	// TODO: Document this method
 	public void set(Collection<Expression> exps) {
 		_exps.clear();
 		_exps.addAll(exps);
 	}
 
+	// TODO: Document this method
 	public void setIdentifierValue(Identifier id, Literal value) {
 		_parent.setIdentifierValue(id, value);
 	}
 
+	// TODO: Document this method
 	public Literal getIdentifierValue(Identifier id) {
 		return _parent.getIdentifierValue(id);
 	}
 
+	// TODO: Document this method
 	public Literal execute() {
 		Literal retval = null;
 		for (Expression line : _exps) {
@@ -57,6 +66,7 @@ public class Program implements Serializable {
 		return retval;
 	}
 
+	// TODO: Document this method
 	public String getAsText() {
 		String result = "";
 		for (Expression exp : _exps) {
@@ -65,6 +75,7 @@ public class Program implements Serializable {
 		return result;
 	}
 
+	// TODO: Document this method
 	public int getNumExpressions() {
 		return _exps.size();
 	}
